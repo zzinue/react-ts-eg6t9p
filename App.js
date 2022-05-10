05b00189d1e91e80338de04bb291f4ace5f3f958
zzinue <zzinue@gmail.com>
2022-05-10 08:48:50 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,143 @@
+import React, { createRef } from 'react';
+import './style.scss';
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Registro </h1>
+      <InputControlado />
+    </div>
+  );
+}
+
+const Style = {
+  Error: {
+    border: '2px solid red',
+  },
+  Ok: {
+    border: '2px solid green',
+  },
+};
+
+//Refactorizar el componete YA
+// Evaluar que la Contraseña tenga mas de 8 caracteres Ya
+// habilitar el boton solo cuando tenga la informacion correcta
+
+// Manejar diferentes mensajes de error dependiendo de input que lo arroje
+
+const InputControlado = () => {
+  const [Name, setName] = React.useState(null);
+  const [ErrorMessage, setErrorMessage] = React.useState('');
+  const [Password, setPassword] = React.useState({
+    Message: '',
+    Type: '',
+    Class: '',
+  });
+
+  const handleInfo = (e) => {};
+  return (
+    <div>
+      <input
+        className={ErrorMessage.Class}
+        type="text"
+        placeholder="Nombre"
+        style={Name?.length >= 3 ? Style.Ok : Style.Error}
+        onChange={({ target }) => {
+          setName(target.value);
+          {
+            target.value.length < 3
+              ? setErrorMessage({
+                  ...ErrorMessage,
+                  Message: 'Nombre muy Corto',
+                  Class: 'Error',
+                })
+              : setErrorMessage({ ...ErrorMessage, Message: '', Class: 'Ok' });
+          }
+        }}
+      />
+      <p>{Error}</p>
+      <input
+        style={Password?.length >= 8 ? Style.Ok : Style.Error}
+        type="password"
+        placeholder="password"
+        onChange={({ target }) => {
+          setPassword(target.value);
+          {
+            target.value.length < 8
+              ? setErrorMessage('Contraseña muy corta')
+              : setErrorMessage('');
+          }
+        }}
+      />
+      <p className="ErrorMessage">{ErrorMessage.Message}</p>
+      <button onClick={handleInfo}>show</button>
+    </div>
+  );
+};
+
+/**
+ * const InputNoControlado = () => {
+  const Name = React.createRef();
+  const Password = createRef();
+
+  const handleInfo = () => {
+    console.log(Name);
+    //petecion POST
+  };
+
+  return (
+    <>
+      <input type="text" ref={Name} placeholder="Nombre" />
+      <input type="password" ref={Password} />
+      <button onClick={handleInfo}>show</button>
+    </>
+  );
+};
+
+const FormularioNoControlado = () => {
+  const handleInfo = (e) => {
+    e.preventDefault();
+    const user = e.target[0].value;
+    const password = e.target[1].value;
+  };
+
+  return (
+    <form onSubmit={handleInfo}>
+      <input type="text" />
+      <input type="password" />
+      <button>show</button>
+    </form>
+  );
+};
+
+const FormularioControlado = () => {
+  const [user, setUser] = React.useState({
+    Name: null,
+    Password: null,
+  });
+
+  const handleInfo = (e) => {
+    e.preventDefault();
+
+    const Name = e.target[0].value;
+    const Password = e.target[1].value;
+
+    setUser({
+      Name,
+      Password,
+    });
+  };
+
+  console.log(user);
+
+  return (
+    <form onSubmit={handleInfo}>
+      <input type="text" />
+      <input type="password" />
+      <button>show</button>
+    </form>
+  );
+};
+ */
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-eg6t9p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-eg6t9p)
